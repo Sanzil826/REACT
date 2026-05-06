20b60187dd6a63515a236a482e3422beae1e0c83
SAnzil107 <be2022se826@gces.edu.np>
2026-05-06 16:40:20 +0800

    project done

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,16 @@
+import Card from "./components/card"
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="parent">
+      <Card user='Botvai' img='https://images.unsplash.com/photo-1777229514251-f946a6833921?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+      <Card user='Sarthak' img='https://images.unsplash.com/photo-1777195680745-9fb67291b216?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+      <Card user='Messi' img='https://images.unsplash.com/photo-1776498534251-64152653c55c?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
     </div>
+   
+
   )
 }
 
 export default App
+ 
