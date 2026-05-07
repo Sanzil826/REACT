6f461fef3ba657af50682bdbd3d6a916bd98ad6f
SAnzil107 <be2022se826@gces.edu.np>
2026-05-07 15:08:20 +0800

    trying module css

---
+++ css/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css"
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className={styles.h1}>This is header</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ css/src/components/button/Button.jsx
@@ -0,0 +1,11 @@
+import styles from "./Button.module.css"
+
+const Button = () => {
+  return (
+    <div className={styles.button}>
+      This is a button
+    </div>
+  )
+}
+
+export default Button
