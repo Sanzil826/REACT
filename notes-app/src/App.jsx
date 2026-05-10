0f2edfe5b18c8fc016a4babf20eec4e1fc2262f9
SAnzil107 <be2022se826@gces.edu.np>
2026-05-11 01:47:39 +0800

    notes-app completed

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -8,10 +8,21 @@ const App = () => {
   const submitHandler=(e)=>{
     e.preventDefault()
     
-    const copyTask =[]
-
+    const copyTask =[...task];
+    copyTask.push({title, desc})
+    setTask(copyTask)
+    console.log(task);
+    
+    
+    
     setTitle('')
     setDesc('')
+  }
+
+  const deleteNote=(idx)=>{
+    const copyTask = [...task];
+    copyTask.splice(idx,1)
+    setTask(copyTask)
   }
 
   return (
@@ -39,20 +50,27 @@ const App = () => {
           setDesc(e.target.value)
         }}/>
 
-        <button className="bg-white w-full font-medium outline-none text-black px-5 py-2 rounded">Add Notes</button>
+        <button className="bg-white active:bg-gray-300 w-full font-medium outline-none text-black px-5 py-2 rounded">Add Notes</button>
       </form>
 
       <div className=" lg:w-1/2 lg:border-l  p-10">
 
         <h1 className="text-3xl font-bold">Your Notes</h1>
 
-        <div className="flex gap-5 h-full overflow-auto bg-gray-900 flex-wrap mt-5">
-          <div className="h-52 w-40 rounded-2xl bg-white"></div>
+        <div className=" flex gap-5 h-[90%] overflow-auto bg-gray-900 flex-wrap items-start justify-start mt-5">
 
-        <div className="h-52 w-40 rounded-2xl bg-white"></div>
-
-        <div className="h-52 w-40 rounded-2xl bg-white"></div>
-
+          {task.map(function(elem,idx){
+            
+            return (<div key={idx} className="relative flex flex-col items-start justify-between h-52 bg-cover w-40 rounded-2xl bg-[url('https://static.vecteezy.com/system/resources/thumbnails/010/793/873/small/a-lined-note-paper-covered-with-transparent-tape-on-a-yellow-background-with-a-white-checkered-pattern-free-png.png')] text-black p-4 bg-white">
+              <div>
+              <h3 className="leading-tight text-xl font-bold">{elem.title}</h3>
+              <p className="mt-2 leading-tight font-medium text-gray-700">{elem.desc}</p>
+              </div>
+              <button onClick={()=>{
+                deleteNote(idx)
+              }} className="w-full cursor-pointer active:scale-95 bg-red-600 py-1 text-xs rounded font-bold text-white">Delete</button>
+            </div>)
+          })}
         </div>
       </div>
     </div>
