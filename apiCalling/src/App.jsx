9e2f479c8ae1ea4bce0a75b99c039137e6efec74
SAnzil107 <be2022se826@gces.edu.np>
2026-05-12 01:22:44 +0800

    learned using axios api calling

--- apiCalling/src/App.jsx
+++ apiCalling/src/App.jsx
@@ -1,21 +1,34 @@
+import axios from 'axios'
+import { useState } from 'react'
 
 const App = () => {
+
+  const [data, setData] = useState([])
   // async function getData(){
   //  const response= await fetch('https://jsonplaceholder.typicode.com/todos/1')
 
   //  console.log(response); 
   // }
 
-  const getData = async()=>{
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  // const getData = async()=>{
+  //   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
 
-    const data = await response.json()
-    console.log(data);   
-  } 
+  //   const data = await response.json()
+  //   console.log(data);   
+  // } 
+
+  const getData =async()=>{
+    const {data} = await axios.get('https://picsum.photos/v2/list')
+    
+    setData(data)
+  }
 
   return (
     <div>
       <button onClick={getData}>Get data</button>
+      {data.map((elem,idx)=>{
+        return <h3 key={idx}>Hello {elem.author}, {idx}</h3>
+      })}
     </div>
   )
 }
